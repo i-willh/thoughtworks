3328c0e5597839be90ec796d5e037c1e81d976b9
Wei Huang <weihuang@CNweihuang.local>
2017-03-07 09:56:09 +0800

    modify Application.java file

--- SpringBootTest/src/main/java/com/qyihu/study/springboot/Application.java
+++ SpringBootTest/src/main/java/com/qyihu/study/springboot/Application.java
@@ -11,6 +11,7 @@ public class Application {
 
 
     public static void main(String[] args) {
+        System.out.println("start execute main method...");
         SpringApplication.run(Application.class, args);
     }
 }
